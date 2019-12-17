535f1d6f10ebae3ddf4cda6aeb68a2652d04bc58
Rutakayile Samuel <samrutakayile@awesomity.rw>
2019-12-17 06:36:53 +0200

    Initial Setup

---
+++ server/app.js
@@ -0,0 +1,14 @@
+/** Imports */
+import express from "express";
+import env from "custom-env";
+
+env.env();
+/** Variable Definitions */
+const app = express();
+const port = process.env.PORT || 3000;
+
+/** Routes */
+app.get("/", (req, res) => res.send("Hey there Dev!"));
+
+/** Server Run */
+app.listen(port, () => console.log(`Dev Social is now running`));
