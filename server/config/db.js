2cdfa35ca63be17b83b78d094a3837c4cd3d11e7
rutaks <samrutakayile@awesomity.rw>
2019-12-18 23:05:28 +0200

    Added DB Connection Handler

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 import env from "custom-env";
 
 env.env();
-const env = process.env.NODE_ENV;
-const db = env === "testing" ? process.env.TEST_DB : process.env.PRODUCTION_DB;
+const environment = process.env.NODE_ENV;
+/* istanbul ignore next */
+const db =
+  environment === "testing" ? process.env.TEST_DB : process.env.PRODUCTION_DB;
 
 /* istanbul ignore next */
 const connectDB = async () => {
