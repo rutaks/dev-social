3f4276821685c3564339fd6c09a5b7f32f11b2ae
rutaks <samrutakayile@awesomity.rw>
2019-12-18 23:24:22 +0200

    Added DB Config To App

--- server/app.js
+++ server/app.js
@@ -1,29 +1,61 @@
+/* istanbul ignore next */
 /** Imports */
 import express from "express";
 import env from "custom-env";
-import Calculator from "./util/calculator";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import Response from "./util/response";
+import api from "./routes/api.route";
+import connect from "./config/db";
+import response from "./util/response";
+import logger from "morgan";
 
 env.env();
 
 /** Variable Definitions */
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
-app.use(express.json());
+/** Middleware Config */
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 /** Routes */
+/**
+ * Route serving Welcome Response
+ * @name /
+ * @function
+ * @param {string} path - Express path
+ */
 app.get("/", (req, res) => Response.send201(res, "Welcome To Social Dev", {}));
 
-app.post("/add", (req, res) => {
-  const { num1, num2 } = req.body;
-  const sum = Calculator.add(num1, num2);
-  Response.send201(res, "success", { sum: sum });
+/**
+ * Route serving API Routes
+ * @name /api/v1
+ * @function
+ * @param {string} path - Express path
+ */
+app.use("/api/v1", api);
+
+/** Error Handling */
+app.use((error, req, res, next) => {
+  res.send404(res, error.message);
 });
 
 /** Server */
-app.listen(port, () =>
-  console.log(`Dev Social is now running on Port: ${port}`)
-);
+const runServer = async () => {
+  try {
+    await connect();
+    app.listen(port, () =>
+      console.debug("INFO:", `Dev Social is now running on Port: ${port}`)
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
+runServer();
 
 export default app;
