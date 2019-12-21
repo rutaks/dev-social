141cb4daa001b56e7c102928826bb7361901de98
rutaks <samrutakayile@awesomity.rw>
2019-12-21 11:03:01 +0200

    Added Delete Route

--- server/routes/api/post.route.js
+++ server/routes/api/post.route.js
@@ -5,10 +5,18 @@ import isLoggedIn from "../../middlewares/isLoggedIn";
 const router = Router();
 
 /**
- * Route handling account creation of a user {POST}
- * @name /api/v1/signup
+ * Route handling post creation of a user {POST}
+ * @name /api/v1/posts
  * @param {string} path - Express path
  */
 router.post("/", isLoggedIn, PostController.save);
+/**
+ * Route Listing of all posts  {GET}
+ * @name /api/v1/posts
+ * @param {string} path - Express path
+ */
+router.get("/", isLoggedIn, PostController.getAllPosts);
+router.get("/:id", isLoggedIn, PostController.getOnePost);
+router.delete("/:id", isLoggedIn, PostController.removePost);
 
 export default router;
