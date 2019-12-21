374e9023e4555b79dd7a19f680708b5e2aa068d6
rutaks <samrutakayile@awesomity.rw>
2019-12-21 20:38:21 +0200

    Added Post Modification Route

--- server/routes/api/post.route.js
+++ server/routes/api/post.route.js
@@ -16,7 +16,23 @@ router.post("/", isLoggedIn, PostController.save);
  * @param {string} path - Express path
  */
 router.get("/", isLoggedIn, PostController.getAllPosts);
+/**
+ * Route Listing of specific post By ID  {GET}
+ * @name /api/v1/posts/:id
+ * @param {string} path - Express path
+ */
 router.get("/:id", isLoggedIn, PostController.getOnePost);
+/**
+ * Route Deleting of specific post by ID  {DELETE}
+ * @name /api/v1/posts/:id
+ * @param {string} path - Express path
+ */
 router.delete("/:id", isLoggedIn, PostController.removePost);
+/**
+ * Route Modifying of specific post by id  {PUT}
+ * @name /api/v1/posts/:id
+ * @param {string} path - Express path
+ */
+router.put("/:id", isLoggedIn, PostController.modifyPost);
 
 export default router;
