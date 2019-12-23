2cd8814f4200cb7c5e442d8f6ea6a77d7a91336f
rutaks <samrutakayile@awesomity.rw>
2019-12-23 18:27:02 +0200

    Handled Deprecation Warning

--- server/config/db.js
+++ server/config/db.js
@@ -13,7 +13,8 @@ const connectDB = async () => {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      useFindAndModify: false
     });
   } catch (err) {
     throw err;
